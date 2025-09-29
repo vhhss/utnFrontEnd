93431b2e001edc3d1b33cfcae074d28db685faa5
vhhss <monstersapbe@gmail.com>
2025-09-29 12:18:53 -0300

    clase9

--- clase9/ejer_clase09/js/manejadora.js
+++ clase9/ejer_clase09/js/manejadora.js
@@ -1,9 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const mensajes ={
+
+        "nombre":{valueMissing:"Por favor ingrese su nombre."},
+        "email":{valueMissing:"", typeMismatch: ""},
+        "clave":{valueMissing:"",tooShort:"",tooLong:""},
+        "pais":{valueMissing:""},
+        "terminos":{valueMissing:""}
+
+    }
+
     const form = document.getElementById('formulario');
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
+
+        // Mostrar los ids y analizar el objeto validityState de los campos inválidos
+        const invalidFields = form.querySelectorAll(":invalid");
+        invalidFields.forEach(campo => {
+            if (campo.id) {
+                console.log("Campo inválido:", campo.id);
+                console.log("validityState:", campo.validity);
+                establecerError(campo.id)
+            }
+        });
 
         if(form.checkValidity()) {
             const obj = {};
@@ -19,5 +39,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         form.classList.add('was-validated');
     });
+
+    function establecerError(id){
+    let divError = document.getElementById("error-"+id);
+    const campo = document.getElementById(id);
+    for(const error in campo.validity){
+        if(campo.validity[error] == true && mensajes[id][error]){
+            divError.innerHTML = mensajes[id][error]
+            break
+        }
+    }
+}
     
 });
+
